69224790b9bdfeed20c202bd2beba31e925b0e7c
Karlis Berzins <berzinsh.karlis@gmail.com>
2023-03-16 06:22:26 +0200

    Initial commit

---
+++ weather/weatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  weatherApp.swift
+//  weather
+//
+//  Created by Karlis Berzins on 16/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct weatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
